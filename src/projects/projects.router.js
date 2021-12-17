08117143468353c77f80b7442cca1d8236adcec1
Blizzcane <blizzcane@gmail.com>
2021-12-16 21:28:41 -0600

    relation error in projects table

--- src/projects/projects.router.js
+++ src/projects/projects.router.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const controller = require("./projects.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-
-
-router.route("/").get(controller.list).all(methodNotAllowed);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
